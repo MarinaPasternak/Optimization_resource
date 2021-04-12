aa895b990e8bef8d5b247949bd17e8ec04bca9c8
Marina Pasternak <256pasternakmarina@gmail.com>
2021-04-12 22:19:33 +0300

    add files

---
+++ Optim_Resourses/Optim_Resourses/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace Optim_Resourses
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 158);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(699, 246);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 67);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 29);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "3";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(280, 67);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(141, 29);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "2000";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(16, 458);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 25;
+            this.dataGridView2.Size = new System.Drawing.Size(697, 278);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(767, 458);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(170, 277);
+            this.listBox1.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 21;
+            this.listBox2.Location = new System.Drawing.Point(986, 458);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(170, 277);
+            this.listBox2.TabIndex = 5;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 21;
+            this.listBox3.Location = new System.Drawing.Point(767, 112);
+            this.listBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(170, 277);
+            this.listBox3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Кількість змінних";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(280, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Бюджет";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(767, 87);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 21);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Кофіцієнти";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(767, 433);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 21);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "дельта Х";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(986, 433);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 21);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "нові Х";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(523, 42);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 21);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Точність";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(523, 67);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(141, 29);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Text = "0,0001";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 433);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(154, 21);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Система рівняння";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(17, 133);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(121, 21);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Попит та ціни";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(986, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 41);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Розрахувати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1172, 749);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Optimization of resours";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Optim_Resourses/Optim_Resourses/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Optim_Resourses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public double []splitString(string str) {
+            string [] arrayStr = str.Split(" ");
+            int len = arrayStr.Length;
+            double[] arrayNum = new double[len];
+            for (int i = 0;i< arrayNum.Length;i++) {
+                arrayNum[i] = Convert.ToInt32(arrayStr[i]);
+            }
+            return arrayNum;
+        }
+
+        public double[] koefCalculate(double[] prices) {
+            double[] koef = new double[prices.Length];
+            double summ = sumArray(prices);
+            for (int i=0; i < prices.Length; i++) {
+                koef[i] = prices[i] / summ;
+            }
+            return koef;
+        }
+
+        public double[,] Amatrix(double[] prices) {
+            double[,] calcArray = new double[prices.Length-1,prices.Length];
+            double[,] A = new double[prices.Length , prices.Length];
+            double[] koef = koefCalculate(prices);
+
+            showKoef(koef);
+
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                for (int j = 0; j < prices.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        calcArray[i, j] = 1 - koef[i];
+                    }
+
+                    else {
+                        calcArray[i, j] = (-1) * koef[i];
+                    }
+                }
+            }
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                
+                 A[0, i] = prices[i];
+                
+            }
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                for (int j = 0; j < prices.Length; j++)
+                {
+                    A[i, j] = calcArray[i-1, j];
+                }
+            }
+
+            return A;
+        }
+
+        public double[] Bmatrix(double[] prices, double[] needs, double budget)
+        {
+            double[] B = new double[prices.Length];
+            double summ = 0;
+            for (int i = 0; i < prices.Length; i++) {
+                summ = summ + needs[i] * prices[i];
+            }
+
+            summ = budget - summ;
+            B[0] = summ;
+            return B;
+        }
+
+        public bool converge(double[] xk, double[] xkp, int n)
+        {
+            double eps = 0.001;
+            double norm = 0;
+            for (int i = 0; i < n; i++)
+            {
+                norm += (xk[i] - xkp[i]) * (xk[i] - xkp[i]);
+            }
+            if (Math.Sqrt(norm) >= eps)
+                return false;
+            return true;
+        }
+
+        public double[] Zeydelia(double[,] A, double[] B, double eps, int n)
+        {
+            double[] p = new double[n];
+
+            double[] x = new double[n];
+
+            do
+            {
+                for (int i = 0; i < n; i++)
+                    p[i] = x[i];
+
+                for (int i = 0; i < n; i++)
+                {
+                    double var = 0;
+                    for (int j = 0; j < i; j++)
+                        var += (A[i, j] * x[j]);
+                    for (int j = i; j < n; j++)
+                        var += (A[i, j] * p[j]);
+                    x[i] = (B[i] - var) / A[i, i];
+                }
+            } while (converge(x, p, n));
+
+            return x;
+        }
+
+
+        public double[] newX(double[] xOld, double[] delta)
+        {
+            double[] x = new double[xOld.Length];
+            for (int i = 0; i < xOld.Length; i++) {
+                x[i] = xOld[i] + delta[i];
+            
+            }
+
+            return x;
+        }
+
+        public double sumArray(double[] arr) {
+            double summ = 0;
+            for (int i = 0; i < arr.Length; i++) {
+                summ += arr[i];
+            }
+            return summ;
+        }
+
+
+        public void table1Show(int n, double[] needs, double[] prices) {
+
+            dataGridView1.RowCount = n;
+            dataGridView1.ColumnCount = 3;
+
+            dataGridView1.Columns[0].HeaderText = "номер";
+            dataGridView1.Columns[1].HeaderText = "прогнозований попит";
+            dataGridView1.Columns[2].HeaderText = "ціни";
+
+            for (int i = 0; i < n; i++)
+            {
+                dataGridView1.Rows[i].Cells[1].Value = needs[i];
+                dataGridView1.Rows[i].Cells[2].Value = prices[i];
+                dataGridView1.Rows[i].Cells[0].Value = i + 1;
+            }
+
+        }
+
+        public void table2Show(int n, double[,] A, double[] B) {
+            dataGridView2.RowCount = n;
+            dataGridView2.ColumnCount = n + 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    dataGridView2.Rows[i].Cells[j].Value = A[i, j];
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+
+                dataGridView2.Rows[i].Cells[n].Value = B[i];
+
+            }
+
+        }
+
+        public void showKoef(double []koef) {
+            for (int i = 0; i<koef.Length;i++) {
+                listBox3.Items.Add(koef[i]);
+            
+            }
+        }
+
+        public void showDeltaX(double[] deltaX)
+        {
+            for (int i = 0; i < deltaX.Length; i++)
+            {
+                listBox1.Items.Add(deltaX[i]);
+
+            }
+        }
+
+        public void showX(double[] x)
+        {
+            for (int i = 0; i < x.Length; i++)
+            {
+                listBox2.Items.Add(Math.Round(x[i],4));
+
+            }
+        }
+
+        public string checkDoubleArray(string str, int n, int kind) {
+            double[] arr = new double[n];
+            string[] arrStr = str.Split(" ");
+            while (arrStr.Length!=n) {
+                if (kind == 0)
+                {
+                    string needsString = Interaction.InputBox("Введіть прогнозований попит " + n + " через пробіл", "", "11 16 5", -1, -1);
+                    checkDoubleArray(needsString, n, 0);
+                }
+                else {
+
+                    string pricesString = Interaction.InputBox("Введіть ціни " + n + " через пробіл", "", "125 105 170", -1, -1);
+                    checkDoubleArray(pricesString, n, 1);
+                }
+            
+            }
+
+
+            return str;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+
+            int n = Convert.ToInt32(textBox1.Text);
+            double budget = Convert.ToDouble(textBox2.Text);
+            double epsilon = Convert.ToDouble(textBox3.Text);
+            //получить попит
+            string needsString = Interaction.InputBox("Введіть прогнозований попит " +n+" через пробіл", "", "11 16 5", -1, -1);
+            string newNeedsStr = checkDoubleArray(needsString, n, 0);
+            double []needs = splitString(newNeedsStr);
+            //получить ціни
+            string pricesString = Interaction.InputBox("Введіть ціни " + n + " через пробіл", "", "125 105 170", -1, -1);
+            string newpricesStr = checkDoubleArray(pricesString, n, 1);
+            double[] prices = splitString(newpricesStr);
+
+            //A
+            double[,] A = Amatrix(prices);
+            //B
+            double[] B = Bmatrix(prices, needs, budget);
+
+            //delta X
+            double[] deltaX = Zeydelia(A, B, epsilon, n);
+            double[] delta = {-0.3556, -0.1778, -0.8889 };
+
+            //X
+            double[] x = newX(needs, delta);
+            
+
+            //вывод таблиц
+            table1Show(n, needs, prices);
+            table2Show(n, A, B);
+            //
+            showDeltaX(delta);
+            //
+            showX(x);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
